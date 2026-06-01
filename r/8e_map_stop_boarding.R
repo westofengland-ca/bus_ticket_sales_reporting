81e1364f9945abeac930d353477e467d0a300c48
tomalx <tomalx14@gmail.com>
2026-06-01 13:36:02 +0100

    Update 8e_map_stop_boarding.R

--- r/8e_map_stop_boarding.R
+++ r/8e_map_stop_boarding.R
@@ -18,7 +18,35 @@ pax_1 <- pax_1 %>%
 pax_1_boilerplate <- pax_1 %>%  # boilerplate example for filling NA gaps
 # this can be used when some GTFS trip start times do not match the Ticketer
 # trip start times.
+  ungroup() %>% 
   drop_na() %>% 
+  select(Bus_Stop_Atco, route_id, shape_id, direction_id, stop_sequence, 
+          stop_name, geometry, route_long_name) %>%
+  # select(direction_id, stop_id,
+  #        ) %>%
+  group_by(route_id, direction_id, Bus_Stop_Atco, stop_sequence, shape_id, route_long_name) %>% 
+  count() %>%
+  ungroup() %>% 
+  group_by(Bus_Stop_Atco) %>% 
+  slice_max(n) %>% 
+  ungroup() %>% 
+  left_join(gtfs_sf$stops, by = c("Bus_Stop_Atco" = "stop_code")) %>% 
+  select(-wheelchair_boarding, -location_type, -parent_station,
+        -platform_code, -zone_id, -n)
+
+
+pax_1_full <- pax_1 %>% 
+  drop_na()
+
+pax_1_na <- pax_1 %>% filter(if_any(everything(), is.na))
+pax_1_na__ <- pax_1_na %>% 
+  select(-route_id, -shape_id, -direction_id, -stop_sequence, -stop_id,
+         -stop_name, -geometry, -route_long_name) %>% 
+  left_join(pax_1_boilerplate, by = c("Bus_Stop_Atco"))
+
+pax_1_full <- rbind(pax_1_full, pax_1_na__)
+
+
   
 ## maybe just nead a stop_direction var 0 or 1 ???
 
